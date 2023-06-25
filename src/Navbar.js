82ae9a81f90f173d16324190df177743c6e39fdf
Firas <firas@crmc.co.il>
2023-06-25 19:25:17 +0300

    init

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+    <nav className="navbar">
+        <h1>Firas Blog</h1>
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/Create" style={{
+                color:"white",
+                backgroundColor:"#f1356d",
+                borderRadius:"8px",
+            }}>Create Blog + </a>
+        </div>
+    </nav>  );
+}
+ 
+export default Navbar;
