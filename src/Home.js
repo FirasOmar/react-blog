72536754d8e08dc24605ab11c5359ff652cdbed4
Firas <firas@crmc.co.il>
2023-06-27 15:53:43 +0300

    Handiling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,29 @@ const Home = () => {
 
     const [Blogs, setBlogs] = useState(null);
     const [isPending,setIsPending]=useState(true);
-   
+    const[error,setError]=useState(null);
     useEffect(()=>{
        fetch('http://localhost:8000/blogs')
        .then(res=>{
+        if(!res.ok){
+            throw Error("Couldn't fetch data from server!");
+        }
         return res.json();
        })
        .then(data =>{
         setBlogs(data);
         setIsPending(false);
-       })
+        setError(null);
+       }).catch((err)=>{
+        setIsPending(false);
+        console.log(err.message)
+        setError(err.message);
+    })
     },[]);
     return (
         <div className="home">
             {isPending && <div>Loading...</div>}
+            {error && <p style={{color:"red",fontSize:"large",fontWeight:"bold"}}>{error}</p>}
       {Blogs && <BlogList blogs={Blogs} title='All Blogs'></BlogList>}
         </div>
     );
