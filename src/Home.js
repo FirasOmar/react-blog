7769a7da0e69f0ed0a701d025445149b60f49418
Firas <firas@crmc.co.il>
2023-06-27 12:55:53 +0300

    fetch data from json file

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,24 @@
+import { useState,useEffect } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
-    return ( 
-    <div className="home">
-        <h2>Home Page:</h2>
-    </div> 
+
+    const [Blogs, setBlogs] = useState(null);
+
+   
+    useEffect(()=>{
+       fetch('http://localhost:8000/blogs')
+       .then(res=>{
+        return res.json();
+       })
+       .then(data =>{
+        setBlogs(data);
+       })
+    },[]);
+    return (
+        <div className="home">
+      {Blogs && <BlogList blogs={Blogs} title='All Blogs'></BlogList>}
+        </div>
     );
 }
- 
+
 export default Home;
