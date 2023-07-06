37b84a59139706f5ac4117c4c95b31e583a78540
Firas <firas@crmc.co.il>
2023-07-06 15:24:04 +0300

    add routing

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,15 @@
+import {Link}from 'react-router-dom'
 const Navbar = () => {
     return (
     <nav className="navbar">
         <h1>Firas Blog</h1>
         <div className="links">
-            <a href="/">Home</a>
-            <a href="/Create" style={{
+            <Link to="/">Home</Link>
+            <Link to="/Create" style={{
                 color:"white",
                 backgroundColor:"#f1356d",
                 borderRadius:"8px",
-            }}>Create Blog + </a>
+            }}>Create Blog + </Link>
         </div>
     </nav>  );
 }
